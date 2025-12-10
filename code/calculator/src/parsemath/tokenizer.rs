40d3126d78bb69b354dc97b4adadddcb8b1a6cb1
Krystian Piekos <krystian.piekos@infotraining.pl>
2025-12-10 13:37:35 +0100

    Calculator - tokenizing operators

--- code/calculator/src/parsemath/tokenizer.rs
+++ code/calculator/src/parsemath/tokenizer.rs
@@ -4,6 +4,11 @@ use std::str::Chars;
 #[derive(Debug, PartialEq)]
 pub enum Token {
     Plus,
+    Minus,
+    Star,
+    Slash,
+    LeftParen,
+    RightParen
 }
 
 #[derive(Debug, PartialEq)]
@@ -28,6 +33,11 @@ impl<'a> Iterator for Tokenizer<'a> {
         if let Some(c) = self.expr.next() {
             match c {
                 '+' => Some(Ok(Token::Plus)),
+                '-' => Some(Ok(Token::Minus)),
+                '*' => Some(Ok(Token::Star)),
+                '/' => Some(Ok(Token::Slash)),
+                '(' => Some(Ok(Token::LeftParen)),
+                ')' => Some(Ok(Token::RightParen)),
                 _ => Some(Err(TokenizingError {})),
             }
         } else {
@@ -39,13 +49,28 @@ impl<'a> Iterator for Tokenizer<'a> {
 #[cfg(test)]
 mod tokenizer_tests {
     use super::*;
+    use rstest::rstest;
 
-    #[test]
-    fn tokenize_operator() {
-        let mut tokenizer = Tokenizer::new("+");
+    #[rstest]
+    #[case("+", Token::Plus)]
+    #[case("-", Token::Minus)]
+    #[case("/", Token::Slash)]
+    #[case("*", Token::Star)]
+    fn tokenize_operator(#[case] expr: &str, #[case] expected_token: Token) {
+        let mut tokenizer = Tokenizer::new(expr);
 
         let result = tokenizer.next();
-        assert_eq!(result, Some(Ok(Token::Plus)));
+        assert_eq!(result, Some(Ok(expected_token)));
         assert_eq!(tokenizer.next(), None);
+    }
+
+    #[rstest]
+    #[case("(", vec![Token::LeftParen])]
+    #[case(")", vec![Token::RightParen])]
+    fn tokenizer_parens(#[case] expr: &str, #[case] expected_tokens: Vec<Token>) {
+        let tokenizer = Tokenizer::new(expr);
+
+        let tokens: Vec<Token> = tokenizer.collect::<Result<Vec<Token>, TokenizingError>>().unwrap();
+        assert_eq!(tokens, expected_tokens);
     }
 }
